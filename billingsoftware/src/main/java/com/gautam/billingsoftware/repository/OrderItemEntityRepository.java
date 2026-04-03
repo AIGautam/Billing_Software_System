2c0dde167f26d8348202ef10bb715b4abff85b61
AIGautam <kumargautam.gk0039@gmail.com>
2026-04-03 21:21:34 +0530

    add awsconfig and securityconfig

---
+++ billingsoftware/src/main/java/com/gautam/billingsoftware/repository/OrderItemEntityRepository.java
@@ -0,0 +1,7 @@
+package com.gautam.billingsoftware.repository;
+
+import com.gautam.billingsoftware.entity.OrderItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemEntityRepository extends JpaRepository<OrderItemEntity, Long> {
+}
